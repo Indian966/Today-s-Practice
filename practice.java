fdc43226afb87100d03550dff8cdc9155934f455
Indian966 <rlaalsrb4175@gmail.com>
2019-01-31 20:43:00 +0900

    Add files via upload

---
+++ practice.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class practice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		System.out.println("혹시 학번이 어떻게 되세요?\n 아 저는.... ");
+		int StdID = scn.nextInt();
+		System.out.println("아하!");
+		System.out.println(StdID);
+		System.out.println("학번이시구나!");
+	}
+
+}
